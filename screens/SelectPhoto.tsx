b1522ae9ca467b1ce7a8b0b96195b74ee65bf033
tiahwang44 <tiahwang44@gmail.com>
2022-05-02 20:22:25 +0900

    [NOMAD] Select Photo 2

--- screens/SelectPhoto.tsx
+++ screens/SelectPhoto.tsx
@@ -1,5 +1,6 @@
-import { Text } from 'react-native';
+import * as MediaLibrary from 'expo-media-library';
 import styled from 'styled-components/native';
+import { useEffect, useState } from 'react';
 
 const Container = styled.View`
   flex: 1;
@@ -17,10 +18,36 @@ const Bottom = styled.View`
 `;
 
 const SelectPhoto = () => {
+  const [ok, setOk] = useState(false);
+  const [photos, setPhotos] = useState<string[]>([]);
+  const getPermissions = async () => {
+    const { accessPrivileges, canAskAgain } =
+      await MediaLibrary.getPermissionsAsync();
+    if (accessPrivileges === 'none' && canAskAgain) {
+      const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync();
+      if (accessPrivileges !== 'none') {
+        setOk(true);
+      }
+    } else if (accessPrivileges !== 'none') {
+      setOk(true);
+    }
+  };
+  const getPhotos = async () => {
+    if (ok) {
+      // const albums = await MediaLibrary.getAlbumsAsync();
+      // console.log(albums);
+      const { assets } = await MediaLibrary.getAssetsAsync();
+      setPhotos(assets.map((asset) => asset.uri));
+    }
+  };
+  useEffect(() => {
+    getPermissions();
+    getPhotos();
+  }, [ok]);
   return (
     <Container>
       <Top />
-      <Bottom />
+      <Bottom></Bottom>
     </Container>
   );
 };

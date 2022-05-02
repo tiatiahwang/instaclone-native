29fe00ab5dc624e296ca9b0ea52c3d22e97b498a
tiahwang44 <tiahwang44@gmail.com>
2022-05-02 23:05:16 +0900

    [NOMAD] Take Photo 1

--- screens/TakePhoto.tsx
+++ screens/TakePhoto.tsx
@@ -1,7 +1,48 @@
-import { Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Camera } from 'expo-camera';
+import styled from 'styled-components/native';
+import { TouchableOpacity } from 'react-native';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const Actions = styled.View`
+  flex: 0.35;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TakePhotoButton = styled.TouchableOpacity`
+  width: 100px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 0.5);
+  border: 1px solid rgba(255, 255, 255, 0.8);
+  border-radius: 50px;
+  opacity: 0.5;
+`;
 
 const TakePhoto = () => {
-  return <Text>TakePhoto</Text>;
+  const [ok, setOk] = useState(false);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const getPermissions = async () => {
+    const { granted } = await Camera.getCameraPermissionsAsync();
+    setOk(granted);
+  };
+  useEffect(() => {
+    getPermissions();
+  }, []);
+  return (
+    <Container>
+      <Camera type={cameraType} style={{ flex: 1 }} />
+      <Actions>
+        <TakePhotoButton></TakePhotoButton>
+        <TouchableOpacity></TouchableOpacity>
+      </Actions>
+    </Container>
+  );
 };
 
 export default TakePhoto;
